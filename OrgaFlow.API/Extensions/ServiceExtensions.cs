be68a27dd86798c4a30d34638c444fd159084acd
Richie_Vist <vistovschiiserghei@gmail.com>
2025-02-06 11:28:45 +0200

    Clean Architecture Separation

---
+++ OrgaFlow.API/Extensions/ServiceExtensions.cs
@@ -0,0 +1,6 @@
+﻿namespace OrgaFlow.Application.Extensions;
+
+public static class ServiceExtensions
+{
+    
+}
---
+++ OrgaFlow.Domain/Entities/User.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace OrgaFlow.Domain.Entities;
+
+public class User: IdentityUser 
+{
+    
+}
